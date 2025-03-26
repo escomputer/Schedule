726482b101a187d5e4889f888db9ff50b854361b
escomputer <nujest307@naver.com>
2025-03-26 16:21:37 +0900

    feat : 생성일자, 수정일자 타입변경
    문자열이 아닌 LocalDateTime 형으로 바꿈

--- src/main/java/com/example/schedule/entity/Schedule.java
+++ src/main/java/com/example/schedule/entity/Schedule.java
@@ -1,23 +1,30 @@
-package com.example.schedule.entitiy;
+package com.example.schedule.entity;
 
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Setter
 public class Schedule {
     private Long id;
     private String task;
     private String author;
-    private String createdAt;
-    private String changedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime changedAt;
     private String password;
 
     public Schedule(String task, String author, String password) {
-
         this.task = task;
         this.author = author;
         this.password = password;
+        this.createdAt = LocalDateTime.now();
+        this.changedAt = LocalDateTime.now();
     }
 }
