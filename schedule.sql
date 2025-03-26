32767bd6916d4fc740d85577a28f64694557d047
escomputer <nujest307@naver.com>
2025-03-27 04:09:13 +0900

    sql init

--- schedule.sql
+++ schedule.sql
@@ -1,12 +1,7 @@
 CREATE TABLE schedules (
-    id int auto_increment primary key,
-    author varchar(255) not null ,
-    task varchar(255) not null ,
-    password varchar(255) not null ,
-    created_at datetime not null ,
-    changed_at datetime not null );
-
-alter table schedules modify column changed_at datetime default current_timestamp on update current_timestamp;la
-ㅁㅁ
-desc schedules;
-tla
+                           id int auto_increment primary key,
+                           author varchar(255) not null ,
+                           task varchar(255) not null ,
+                           password varchar(255) not null ,
+                           created_at datetime default current_timestamp,
+                           updated_at datetime default current_timestamp on update current_timestamp);
