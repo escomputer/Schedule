912f658cb5792449a1b053eced91452f3554c6b5
escomputer <nujest307@naver.com>
2025-03-26 16:38:23 +0900

    Feat : DB에 스케줄 저장하는 saveSchedule기능

--- src/main/java/com/example/schedule/repository/ScheduleRepositoryImpl.java
+++ src/main/java/com/example/schedule/repository/ScheduleRepositoryImpl.java
@@ -1,10 +1,14 @@
 package com.example.schedule.repository;
 
-import com.example.schedule.entitiy.Schedule;
+import com.example.schedule.dto.ScheduleResponseDto;
+import com.example.schedule.entity.Schedule;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class ScheduleRepositoryImpl implements ScheduleRepository {
@@ -15,5 +19,25 @@ public class ScheduleRepositoryImpl implements ScheduleRepository {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    @Override
+    public ScheduleResponseDto saveSchedule(Schedule schedule) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("schedules")
+                .usingGeneratedKeyColumns("id");
 
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("task", schedule.getTask());
+        parameters.put("author", schedule.getAuthor());
+        parameters.put("password", schedule.getPassword());
+        parameters.put("created_at", schedule.getCreatedAt());
+        parameters.put("changed_at", schedule.getChangedAt());
+
+        Number key = jdbcInsert.executeAndReturnKey(parameters);
+
+        // ID 값을 Schedule 객체에 세팅하고 반환
+        schedule.setId(key.longValue());
+
+        // ScheduleResponseDto로 변환하여 반환
+        return new ScheduleResponseDto(schedule);
+    }
 }
